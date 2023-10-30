9820aee4bb3045848f79628317b0a2edc773e2b9
Hanh Tran Xuan <hank.txh@gmail.com>
2023-10-30 15:24:17 +0700

    add cicd folder

---
+++ cicd/python-code/app.py
@@ -0,0 +1,6 @@
+def hello_world():
+  """Returns the string "Hello, world!"."""
+  return "Hello, world!"
+
+if __name__ == "__main__":
+  print(hello_world())
---
+++ cicd/python-code/test_deployment.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def test_deployment():
+  """Tests if the application is deployed successfully."""
+
+  # Get the output of the application.
+  output = subprocess.check_output(["docker", "exec", "my-container-name", "python", "app.py"])
+
+  # Assert that the application is running and returning the expected output.
+  assert output == "Hello, world!"
+
+if __name__ == "__main__":
+  test_deployment()
