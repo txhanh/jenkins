4385e46e3c68d486885d7312e8ad0e6ded2a8c8a
Hanh Tran Xuan <hank.txh@gmail.com>
2023-10-30 16:02:18 +0700

    add Dockerfile

--- cicd/python-code/test_deployment.py
+++ cicd/python-code/test_deployment.py
@@ -4,7 +4,7 @@ def test_deployment():
   """Tests if the application is deployed successfully."""
 
   # Get the output of the application.
-  output = subprocess.check_output(["docker", "exec", "my-container-name", "python", "app.py"])
+  output = subprocess.check_output(["docker", "exec", "my-python-app", "python", "app.py"])
 
   # Assert that the application is running and returning the expected output.
   assert output == "Hello, world!"
